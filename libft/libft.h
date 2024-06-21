ed718fd4cb91c565bc20852b965c96e9f8d8bbcf
Angel Diaz Rusiñol <adiaz-ru@car10s3.42barcelona.com>
2024-06-21 19:11:26 +0200

    solved strrchr

--- libft/libft.h
+++ libft/libft.h
@@ -6,31 +6,32 @@
 /*   By: adiaz-ru <adiaz-ru@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 10:14:55 by adiaz-ru          #+#    #+#             */
-/*   Updated: 2024/06/17 13:52:08 by adiaz-ru         ###   ########.fr       */
+/*   Updated: 2024/06/21 19:03:14 by adiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-int		ft_isdigit(int n);
-int		ft_isalpha(char c);
-int		ft_isdigit(int n);
-int		ft_isalnum(int c);
-int		ft_isascii(int ascii);
-int		ft_isprint(int printable);
-int		ft_strlen(const char *str);
-int		ft_strlcat(char *dst, const char *src, unsigned int size);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	*ft_strdup(char *src);
-char	*ft_strchr(const char *s, int c);
-void	*ft_memset(void *array, int value, unsigned int space);
-void	ft_bzero(void *array, unsigned int size);
-void	*ft_memcpy(void *dest, const void *src, unsigned int n);
-void	*ft_memmove(void *dest, const void *src, unsigned int n);
+int				ft_isdigit(int n);
+int				ft_isalpha(char c);
+int				ft_isdigit(int n);
+int				ft_isalnum(int c);
+int				ft_isascii(int ascii);
+int				ft_isprint(int printable);
+int				ft_strlen(const char *str);
+int				ft_strlcat(char *dst, const char *src, unsigned int size);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+char			*ft_strdup(char *src);
+char			*ft_strrchr(const char *s, int c);
+char			*ft_strchr(const char *s, int c);
+void			*ft_memset(void *array, int value, unsigned int space);
+void			ft_bzero(void *array, unsigned int size);
+void			*ft_memcpy(void *dest, const void *src, unsigned int n);
+void			*ft_memmove(void *dest, const void *src, unsigned int n);
 unsigned int	ft_strlcpy(char *dst, const char *src, unsigned int size);
 #endif
