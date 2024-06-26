288489c165199f3fbe3e0c06ae3c01a9eeef3a2e
Angel Diaz Rusiñol <adiaz-ru@car11s3.42barcelona.com>
2024-06-26 20:33:24 +0200

    muchas cosas

---
+++ libft/bonus/ft_lstnew_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adiaz-ru <adiaz-ru@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 20:09:12 by adiaz-ru          #+#    #+#             */
+/*   Updated: 2024/06/26 20:09:14 by adiaz-ru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+	
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (0);
+	node->content = content;
+	node->next = 0;
+	return (node);
+}
