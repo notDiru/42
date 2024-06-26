33c18f47f6c85f6d65139e327eb4b077e93c039d
Angel Diaz Rusiñol <adiaz-ru@car4s5.42barcelona.com>
2024-06-26 15:40:36 +0200

    ft_strmapi

---
+++ libft/sources/ft_itoa.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adiaz-ru <adiaz-ru@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 14:41:54 by adiaz-ru          #+#    #+#             */
+/*   Updated: 2024/06/26 15:00:31 by adiaz-ru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../libft.h"
+
+static int	ft_min(char *str)
+{
+	str[0] = '-';
+	str[1] = '2';
+	return (147483648);
+}
+
+static int	ft_intlen(int n)
+{
+	int	c;
+
+	c = 1;
+	if (n == -2147483648)
+		return (11);
+	if (n < 0)
+	{
+		c++;
+		n *= -1;
+	}
+	while (n / 10 > 0)
+	{
+		n = n / 10;
+		c++;
+	}
+	return (c);
+}
+
+char  *ft_itoa(int n)
+{
+	int	i;
+	char  *result;
+
+	i = ft_intlen(n);
+	result = (char *)ft_calloc(i + 1, sizeof(char));
+	if (!result)
+		return (0);
+	if (n == -2147483648)
+		n = ft_min(result);
+	if (n < 0)
+	{
+		result[0] = '-';
+		n *= -1;
+	}
+	while (n / 10 >= 0)
+	{
+		result[i - 1] = (n % 10) + '0';
+		i--;
+		if (n / 10 == 0)
+			return (result);
+		n = n / 10;
+	}
+	return (result);
+}
---
+++ libft/sources/ft_strmapi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adiaz-ru <adiaz-ru@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 15:11:05 by adiaz-ru          #+#    #+#             */
+/*   Updated: 2024/06/26 15:34:46 by adiaz-ru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../libft.h"
+
+char  *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int	  i;
+	char  *str;
+
+	str = (char *)ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
