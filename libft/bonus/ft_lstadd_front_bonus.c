f450604a866dc7a72501be9073e8e6e2bfcb9fd2
adiaz-ru <adiaz-ru@student.42barcelona.com>
2024-06-27 09:51:19 +0200

    Makefile and bonus

--- libft/bonus/ft_lstadd_front_bonus.c
+++ libft/bonus/ft_lstadd_front_bonus.c
@@ -1,41 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adiaz-ru <adiaz-ru@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/25 10:13:33 by adiaz-ru          #+#    #+#             */
-/*   Updated: 2024/06/25 11:18:43 by adiaz-ru         ###   ########.fr       */
+/*   Created: 2024/06/27 08:43:46 by adiaz-ru          #+#    #+#             */
+/*   Updated: 2024/06/27 08:49:18 by adiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	char	*p;
-	int		s1len;
-	int		s2len;
-	int		i;
-	int		j;
-
-	s1len = ft_strlen(s1);
-	s2len = ft_strlen(s2);
-	p = (char *)malloc((s1len + s2len + 1) * sizeof(char));
-	if (!p)
-		return (0);
-	i = 0;
-	while (i < s1len)
+	if (lst && new)
 	{
-		p[i] = s1[i];
-		i++;
+		new->next = *lst;
+		*lst = new;
 	}
-	j = 0;
-	while (j < s2len)
-	{
-		p[i + j] = s2[j];
-		j++;
-	}
-	p[i + j] = '\0';
-	return (p);
 }
