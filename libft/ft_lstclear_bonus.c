10aa02f67b20f4e212cdaf286cc1b8004c6d14da
notDiru <angeldiazdiru93@gmail.com>
2024-07-12 08:29:01 +0200

    fixed norm

--- libft/ft_lstclear_bonus.c
+++ libft/ft_lstclear_bonus.c
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adiaz-ru <adiaz-ru@student.42barcelona.co  +#+  +:+       +#+        */
+/*   By: adiaz-ru <adiaz-ru@student.42barcelona.com +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 19:32:20 by adiaz-ru          #+#    #+#             */
-/*   Updated: 2024/07/01 19:37:31 by adiaz-ru         ###   ########.fr       */
+/*   Updated: 2024/07/12 08:28:37 by adiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list *temp;
+	t_list	*temp;
 
 	if (!lst || !*lst || !del)
 		return ;
